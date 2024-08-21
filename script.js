3f42dc93e63564b14e1db3b6ecc41d3e57001554
Azhaghiri <ganadhan6528@gmail.com>
2024-08-21 10:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your code here
-it('should find the select element and its label', () => {
-    cy.visit(baseUrl + "/main.html"); // Ensure this path is correct
-    cy.get("form").within(() => {
-        cy.get("select#cars").should('exist'); // Match the id 'cars'
-        cy.get("label[for='cars']").should('exist'); // Match the id 'cars'
-    });
-});
